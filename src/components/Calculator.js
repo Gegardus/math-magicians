11eaacc25411d3d67b6815222297d3b6556a3ea5
Gegardus <stgegard@gmail.com>
2022-02-16 21:19:30 +0400

    Fix: linter errors

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -12,32 +12,32 @@ const Calculator = () => {
   const { next, total } = state;
   return (
     <main>
-        <div className="output"><p className="display">{next || total || 0}</p></div>
-        <div className="buttons">
+      <div className="output"><p className="display">{next || total || 0}</p></div>
+      <div className="buttons">
           <button className="btn" type="button" onClick={handleClick}>AC</button>
           <button className="btn" type="button" onClick={handleClick}>+/-</button>
           <button className="btn" type="button" onClick={handleClick}>%</button>
           <button className="btn end" type="button" onClick={handleClick}>÷</button>
         </div>
-        <div className="buttons">
+      <div className="buttons">
           <button className="btn" type="button" onClick={handleClick}>7</button>
           <button className="btn" type="button" onClick={handleClick}>8</button>
           <button className="btn" type="button" onClick={handleClick}>9</button>
           <button className="btn end" type="button" onClick={handleClick}>x</button>
         </div>
-        <div className="buttons">
+      <div className="buttons">
           <button className="btn" type="button" onClick={handleClick}>4</button>
           <button className="btn" type="button" onClick={handleClick}>5</button>
           <button className="btn" type="button" onClick={handleClick}>6</button>
           <button className="btn end" type="button" onClick={handleClick}>-</button>
         </div>
-        <div className="buttons">
+      <div className="buttons">
           <button className="btn" type="button" onClick={handleClick}>1</button>
           <button className="btn" type="button" onClick={handleClick}>2</button>
           <button className="btn" type="button" onClick={handleClick}>3</button>
           <button className="btn end" type="button" onClick={handleClick}>+</button>
         </div>
-        <div className="buttons">
+      <div className="buttons">
           <button className="btn zero" type="button" onClick={handleClick}>0</button>
           <button className="btn" type="button" onClick={handleClick}>.</button>
           <button className="btn end" type="button" onClick={handleClick}>=</button>
